891bc35d5ee39b59b4c8cf8cea639e4d86f7b85d
Afowowe Olumide Victor <volumide42@gmail.com>
2023-05-09 13:36:45 +0100

    join all data

--- src/pages/placeholder.jsx
+++ src/pages/placeholder.jsx
@@ -1,7 +1,11 @@
+import { useParams } from "react-router-dom"
+
 const Placeholder = () => {
+  const { name } = useParams()
+  console.log(name)
   return (
     <div className="h-screen  flex container mx-auto px-[100px]">
-      <div className="flex-1 border-r h-full py-10">side</div>
+      <div className="flex-1 border-r h-full py-10">{name}</div>
       <div className="flex-auto flex-shrink-0 py-10 px-10">content</div>
     </div>
   )
---
+++ src/utls/subscriptions/index.js
@@ -0,0 +1,15 @@
+import edu from "./edu"
+import electricity from "./electricity"
+import intertenetData from "./internet"
+import airtime from "./phone"
+import subTv from "./tv"
+
+const links = {
+  "phone": airtime,
+  "internet-bill": intertenetData,
+  "tv-subscription": subTv,
+  "electricity-bill": electricity,
+  education: edu
+}
+
+export default links
