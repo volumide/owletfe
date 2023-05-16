a03a892270978c4326f808981306c54b6f17d129
Afowowe Olumide <volumide42@gmail.com>
2023-05-16 18:15:25 +0100

    authentication

--- src/utls/url.js
+++ src/utls/url.js
@@ -1,16 +1,20 @@
 import axios from "axios"
 
-export const getCountries = async (url) => {
-  const request = await axios.get(`${url}get-international-airtime-countries`)
+const password = "Olumide1"
+const userName = "volumide42@gmail.com"
+const url = "https://sandbox.vtpass.com/api/"
+
+export const getCountries = async () => {
+  const request = await axios.get(`${url}get-international-airtime-countries`, { headers: { "Authorization": `Basic ${window.btoa(userName + ":" + password)}` } })
   return request.data
 }
 
-export const getAirtimeProduct = async (url, code) => {
-  const request = await axios.get(`${url}get-international-airtime-product-types?code=${code}`)
+export const getAirtimeProduct = async (code) => {
+  const request = await axios.get(`${url}get-international-airtime-product-types?code=${code}`, { headers: { "Authorization": `Basic ${window.btoa(userName + ":" + password)}` } })
   return request.data
 }
 
-export const getAirtimeOperator = async (url, code) => {
-  const request = await axios.get(`${url}get-international-airtime-operator?code=${code}&product_type_id=4`)
+export const getAirtimeOperator = async (code) => {
+  const request = await axios.get(`${url}get-international-airtime-operator?code=${code}&product_type_id=4`, { headers: { "Authorization": `Basic ${window.btoa(userName + ":" + password)}` } })
   return request.data
 }
