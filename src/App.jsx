531a721517ecea0cb2d27d25f19076e1c634db22
Afowowe Olumide <volumide42@gmail.com>
2023-05-20 09:03:49 +0100

    modify url paths

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,6 @@ import Home from "./pages/home"
 import Container from "./pages/container"
 import Placeholder from "./pages/placeholder"
 import Transaction from "./pages/transaction"
-import TvForm from "./pages/forms/tv-sub.form"
-import Electricity from "./pages/forms/electricity.form"
-import Education from "./pages/forms/education.form"
-import Airtime from "./pages/forms/airtime.form"
-import InterneData from "./pages/forms/data.form"
 
 function App() {
   const router = createBrowserRouter([
@@ -30,26 +25,7 @@ function App() {
           path: "/owlet/:name/:type",
           element: <Placeholder />
         },
-        {
-          path: "/tv-subscription",
-          element: <TvForm />
-        },
-        {
-          path: "/elect-subscription",
-          element: <Electricity />
-        },
-        {
-          path: "/edu-subscription",
-          element: <Education />
-        },
-        {
-          path: "/internet-subscription",
-          element: <InterneData />
-        },
-        {
-          path: "/airtime-subscription",
-          element: <Airtime />
-        },
+
         {
           path: "/transaction",
           element: <Transaction />
