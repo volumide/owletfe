e4e916762dd571f3953227edee0da1eff350f3c8
Afowowe Olumide <volumide42@gmail.com>
2023-05-20 09:05:38 +0100

    create tv form

--- src/pages/forms/tv-sub.form.jsx
+++ src/pages/forms/tv-sub.form.jsx
@@ -1,29 +1,65 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useForm } from "react-hook-form"
 import Input from "../../components/input"
 import tvForn from "../../utls/form/tv-form"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Button from "../../components/button"
+import { getVariationCodes, paySubscripiton, verifyMerchant } from "../../utls/url"
 
 const TvForm = () => {
   const tvForms = tvForn
   const { handleSubmit, control } = useForm()
   const [proceed, setProceed] = useState(false)
-  const submit = (data) => {
+  const query = new URLSearchParams(window.location.search)
+  const queries = Object.fromEntries(query.entries())
+  const [packages, setPackages] = useState([])
+
+  const submit = async () => {
     setProceed(true)
-    console.log(data)
+    console.log("working...")
+    const req = await verifyMerchant({
+      serviceID: "dstv",
+      billersCode: "1212121212"
+    })
+
+    console.log(req)
+    if (!req.content.error) {
+      const sendPay = await paySubscripiton({
+        serviceID: "dstv",
+        billersCode: "1212121212",
+        phone: "08011111111",
+        amount: req.content.Renewal_Amount,
+        subscription_type: ""
+      })
+      console.log(sendPay)
+    }
   }
+
+  useEffect(() => {
+    if (queries.service)
+      getVariationCodes(queries.service).then((e) => {
+        console.log(e)
+        setPackages(e.content.varations)
+      })
+  }, [])
 
   return (
     <>
       <form onSubmit={handleSubmit(submit)}>
         {tvForms.map((i) =>
           i.select ? (
-            <Input label={i.label || "moving"} type={i?.type || "text"} control={control} key={i} name={i.name} select={true}>
-              {i.options.map((e) => (
-                <option value={e.value} key={e.code}>
-                  {e.key}
-                </option>
-              ))}
+            <Input label={i.label || "moving"} type={i?.type || "text"} control={control} key={i.name} name={i.name} select={true}>
+              {i.options
+                ? i.options.map((value) => (
+                    <option value={value.toLocaleLowerCase()} key={value + Math.random}>
+                      {value}
+                    </option>
+                  ))
+                : packages.map((e) => (
+                    <option value={e.variation_amount} key={e.name}>
+                      {e.name}
+                    </option>
+                  ))}
             </Input>
           ) : (
             <Input label={i.label} type={i?.type || "text"} control={control} key={i.label} name={i.name} />
