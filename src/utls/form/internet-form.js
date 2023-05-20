ec61711af1070b1b3b19ff6e5a6be76035445809
Afowowe Olumide <volumide42@gmail.com>
2023-05-20 09:52:09 +0100

    modify internet form

--- src/utls/form/internet-form.js
+++ src/utls/form/internet-form.js
@@ -1,6 +1,6 @@
 const internetForm = [
   { label: "Package", name: "package_type", type: "text", select: true, options: [] },
-  { label: "Amount", name: "amount", type: "text" },
+  { label: "Amount", name: "amount", type: "text", disabled: true },
   { label: "Phone Number", name: "phone", type: "number" },
   { label: "Email Address", name: "email", type: "email" },
   { label: "Number of Pins ", name: "pin", type: "text" }
