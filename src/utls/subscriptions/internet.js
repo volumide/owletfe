c35697126ed3a268fa0baf4e9624e41f989e1c13
Afowowe Olumide <volumide42@gmail.com>
2023-05-20 09:52:29 +0100

    modify data form

--- src/utls/subscriptions/internet.js
+++ src/utls/subscriptions/internet.js
@@ -1,11 +1,11 @@
 const intertenetData = [
-  { name: "airtel", title: "Aitel Data ", logo: "airtel", cat: "phone" },
-  { name: "mtn", title: "MTN Data ", logo: "mtn", cat: "phone" },
-  { name: "glo", title: "GLO Data ", logo: "glo", cat: "phone" },
-  { name: "9mobile", title: "9Mobile Data ", logo: "nineMobile", cat: "phone" },
+  { name: "airtel", title: "Aitel Data ", logo: "airtel", cat: "phone", service: "airtel-data" },
+  { name: "mtn", title: "MTN Data ", logo: "mtn", cat: "phone", service: "mtn-data" },
+  { name: "glo", title: "GLO Data ", logo: "glo", cat: "phone", service: "glo-data" },
+  { name: "9mobile", title: "9Mobile Data ", logo: "nineMobile", cat: "phone", service: "etisalat-data" },
   // { name: "9mobile_pin", title: "9Mobile Data", logo: "", cat:"phone" },
-  { name: "smile", title: "Smile Network Payment", logo: "smile", cat: "phone" },
-  { name: "glo_data", title: "GLO Data SME Airtime", logo: "glo", cat: "phone" }
+  { name: "smile", title: "Smile Network Payment", logo: "smile", cat: "phone", service: "smile-direct" },
+  { name: "glo_data", title: "GLO Data SME Airtime", logo: "glo", cat: "phone", service: "spectranet" }
 ]
 
 export default intertenetData
